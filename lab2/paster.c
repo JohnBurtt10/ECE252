13943811b64459d324799af3356c9c9950877657
e2romero <e2romero@uwaterloo.ca>
2023-09-27 14:52:27 -0400

    Minor changes

--- lab2/paster.c
+++ lab2/paster.c
@@ -6,6 +6,7 @@
 
 #define THREAD_CMD "-t"
 #define RANDOM_PNG_SEG "-n"
+#define BUFF_SIZE 8000 // Each image should be less than 8KB
 
 typedef struct _args {
     int numThreads;
@@ -26,6 +27,8 @@ void curlInit(CURL* curlHandlers[], args* arguments);
 void createRequest();
 void cleanupCurlHandlers(CURL* curlArr[], int numOfCurls);
 void setup_curl_handler(CURL* curlHandler);
+
+RECV_BUF *pngArray;
 
 /* cURL callbacks */
 // Callback that receives header data.
@@ -49,10 +52,12 @@ const char* const urls[3][3] =
     "http://ece252-3.uwaterloo.ca:2520/image?img=3"}
 };
 
+pthread_mutex_t lock; // Mutex lock we will use to lock resources. Aka block other threads from executing some lines
+
 int main(int argc, char* argv[]){
 
     args *arguments = malloc(sizeof(args));
-    RECV_BUF* recvBuffer[3]; // There are 3 urls.
+    pngArray = malloc(((8 + 8 + 4) + (8000)) * 50);
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
@@ -65,7 +70,13 @@ int main(int argc, char* argv[]){
     pthread_t *p_tids = malloc(sizeof(pthread_t) * arguments->numThreads);
 
      // Creating numThreads amount of curl handlers to give to each thread. Want to reuse handlers to improve performance.
-    CURL* curlHandlers = malloc(sizeof(CURL) * arguments->numThreads);
+    CURL* curlHandlers = (CURL*) malloc(sizeof(CURL) * arguments->numThreads);
+
+    // Initilizing Mutex lock
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
 
     printf("numThreads: %d | imageSegment: %d\n", arguments->numThreads, arguments->imageNum);
 
@@ -73,9 +84,10 @@ int main(int argc, char* argv[]){
 
     free(arguments);
     free(p_tids);
+    free(pngArray);
     // free(imageSegBuffer);
-    cleanupCurlHandlers(curlHandlers, arguments->numThreads);
     curl_global_cleanup();
+    cleanupCurlHandlers(curlHandlers, arguments->numThreads);
     free(curlHandlers);
     return 0;
 }
@@ -104,17 +116,19 @@ void curlInit(CURL* curlHandlers[], args* arguments){
 
         /* register write call back function to process received data */
         // curl_easy_setopt(curlHandlers, CURLOPT_WRITEFUNCTION, write_cb_curl); 
-        // /* user defined data structure passed to the call back function */
+        /* user defined data structure passed to the call back function */
         // curl_easy_setopt(curlHandlers, CURLOPT_WRITEDATA, (void *)&pngData);
 
         /* register header call back function to process received header data */
         // curl_easy_setopt(curlHandlers, CURLOPT_HEADERFUNCTION, header_cb_curl); 
-        // /* user defined data structure passed to the call back function */
+        /* user defined data structure passed to the call back function */
         // curl_easy_setopt(curlHandlers, CURLOPT_HEADERDATA, (void *)&pngData);
 
         /* some servers requires a user-agent field */
         curl_easy_setopt(curlHandlers[i], CURLOPT_USERAGENT, "libcurl-agent/1.0");
     }
+
+
 }
 
 size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata){
