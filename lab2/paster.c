d32160268f795889b568249573b0a2e4ba605347
e2romero <e2romero@uwaterloo.ca>
2023-09-27 13:29:57 -0400

    IDK

--- lab2/paster.c
+++ lab2/paster.c
@@ -9,7 +9,7 @@
 
 typedef struct _args {
     int numThreads;
-    int imageSegment;
+    int imageNum;
 } args;
 
 typedef struct recv_buf2 {
@@ -22,9 +22,10 @@ typedef struct recv_buf2 {
 
 void processInput(char *command, char *value, args* destination);
 void initalizeArguments(args* argument);
-void curlInit(CURL* curlHandler, char *url, RECV_BUF* pngData);
+void curlInit(CURL* curlHandlers[], args* arguments);
 void createRequest();
 void cleanupCurlHandlers(CURL* curlArr[], int numOfCurls);
+void setup_curl_handler(CURL* curlHandler);
 
 /* cURL callbacks */
 // Callback that receives header data.
@@ -32,6 +33,21 @@ size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
 // Callback for writing received data.
 size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
 
+const char* const urls[3][3] = 
+{
+    // Server 1
+    {"http://ece252-1.uwaterloo.ca:2520/image?img=1", 
+    "http://ece252-1.uwaterloo.ca:2520/image?img=2", 
+    "http://ece252-1.uwaterloo.ca:2520/image?img=3"},
+    // Server 2
+    {"http://ece252-2.uwaterloo.ca:2520/image?img=1", 
+    "http://ece252-2.uwaterloo.ca:2520/image?img=2", 
+    "http://ece252-2.uwaterloo.ca:2520/image?img=3"},
+    // Server 3
+    {"http://ece252-3.uwaterloo.ca:2520/image?img=1", 
+    "http://ece252-3.uwaterloo.ca:2520/image?img=2", 
+    "http://ece252-3.uwaterloo.ca:2520/image?img=3"}
+};
 
 int main(int argc, char* argv[]){
 
@@ -40,23 +56,26 @@ int main(int argc, char* argv[]){
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    const char* const url[3] = {"http://ece252-1.uwaterloo.ca:2520/image?img=1", 
-                    "http://ece252-1.uwaterloo.ca:2520/image?img=2", 
-                    "http://ece252-1.uwaterloo.ca:2520/image?img=3"};
-
     initalizeArguments(arguments);
+
     for (int i = 1; i < argc - 1; i = i + 2){
         processInput(argv[i], argv[i+1], arguments);
     }
 
     pthread_t *p_tids = malloc(sizeof(pthread_t) * arguments->numThreads);
+
+     // Creating numThreads amount of curl handlers to give to each thread. Want to reuse handlers to improve performance.
     CURL* curlHandlers = malloc(sizeof(CURL) * arguments->numThreads);
 
-    printf("numThreads: %d | imageSegment: %d\n", arguments->numThreads, arguments->imageSegment);
+    printf("numThreads: %d | imageSegment: %d\n", arguments->numThreads, arguments->imageNum);
+
+    curlInit(curlHandlers, arguments);
 
     free(arguments);
+    free(p_tids);
     // free(imageSegBuffer);
     cleanupCurlHandlers(curlHandlers, arguments->numThreads);
+    curl_global_cleanup();
     free(curlHandlers);
     return 0;
 }
@@ -66,26 +85,36 @@ void createRequest(){
 }
 
 // Sets up curl handler for provided url and png data.
-void curlInit(CURL* curlHandler, char *url, RECV_BUF* pngData){
-    curlHandler = curl_easy_init();
+void curlInit(CURL* curlHandlers[], args* arguments){
 
-    if (curlHandler == NULL){
-        printf("Failed to initalize CURL");
-        return;
+    for (int i = 0; i < arguments->numThreads; i++){
+        curlHandlers[i] = curl_easy_init();
+
+        if (curlHandlers[i] == NULL){
+            printf("Failed to initalize CURL");
+            return;
+        }    
+
+        int imageToGrab = arguments->imageNum - 1;
+
+        printf("%s\n", urls[i % 3][imageToGrab]);
+
+        /* specify URL to get */
+        curl_easy_setopt(curlHandlers[i], CURLOPT_URL, urls[i % 3][imageToGrab]);
+
+        /* register write call back function to process received data */
+        // curl_easy_setopt(curlHandlers, CURLOPT_WRITEFUNCTION, write_cb_curl); 
+        // /* user defined data structure passed to the call back function */
+        // curl_easy_setopt(curlHandlers, CURLOPT_WRITEDATA, (void *)&pngData);
+
+        /* register header call back function to process received header data */
+        // curl_easy_setopt(curlHandlers, CURLOPT_HEADERFUNCTION, header_cb_curl); 
+        // /* user defined data structure passed to the call back function */
+        // curl_easy_setopt(curlHandlers, CURLOPT_HEADERDATA, (void *)&pngData);
+
+        /* some servers requires a user-agent field */
+        curl_easy_setopt(curlHandlers[i], CURLOPT_USERAGENT, "libcurl-agent/1.0");
     }
-
-    /* register write call back function to process received data */
-    curl_easy_setopt(curlHandler, CURLOPT_WRITEFUNCTION, write_cb_curl); 
-    /* user defined data structure passed to the call back function */
-    curl_easy_setopt(curlHandler, CURLOPT_WRITEDATA, (void *)&pngData);
-
-    /* register header call back function to process received header data */
-    curl_easy_setopt(curlHandler, CURLOPT_HEADERFUNCTION, header_cb_curl); 
-    /* user defined data structure passed to the call back function */
-    curl_easy_setopt(curlHandler, CURLOPT_HEADERDATA, (void *)&pngData);
-
-    /* some servers requires a user-agent field */
-    curl_easy_setopt(curlHandler, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 }
 
 size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata){
@@ -110,16 +139,16 @@ void processInput(char *command, char *value, args* destination){
     
     int longToInt = stringToLong;
 
-    if (strcmp(command, THREAD_CMD)){
+    if (strcmp(command, THREAD_CMD) == 0){
         destination->numThreads = longToInt;
     }
 
-    if (strcmp(command, RANDOM_PNG_SEG)){
-        destination->imageSegment = longToInt;
+    if (strcmp(command, RANDOM_PNG_SEG) == 0){
+        destination->imageNum = longToInt;
     }
 }
 
 void initalizeArguments(args* argument){
-    argument->imageSegment = 1;
+    argument->imageNum = 1;
     argument->numThreads = 1;
 }
