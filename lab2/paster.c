d10c904f077bcf11e4c85a76e887b331de811324
e2romero <e2romero@uwaterloo.ca>
2023-10-07 13:10:14 -0400

    Finished(?) multithreading

--- lab2/paster.c
+++ lab2/paster.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <curl/curl.h>
 
 #define THREAD_CMD "-t"
 #define RANDOM_PNG_SEG "-n"
-#define BUFF_SIZE 8000 // Each image should be less than 8KB
+#define BUF_SIZE 1048576  /* 1024*1024 = 1M */
+#define BUF_INC  524288   /* 1024*512  = 0.5M */
+#define ECE252_HEADER "X-Ece252-Fragment: "
+
+#define max(a, b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
 
 typedef struct _args {
     int numThreads;
     int imageNum;
 } args;
+
+typedef struct _thread_args {
+    int thread_id;
+    int imageToGrab;
+} THREAD_ARGS;
 
 typedef struct recv_buf2 {
     char *buf;       /* memory to hold a copy of received data */
@@ -21,19 +34,20 @@ typedef struct recv_buf2 {
                      /* <0 indicates an invalid seq number */
 } RECV_BUF;
 
-typedef struct png_buffer {
-    RECV_BUF* pngs;
-    int size;
+typedef struct _png_buffer {
+    char* pngs[50]; // 50 images, each of a buffer of size BUF_SIZE (Aka some char* of size BUF_SIZE)
+    volatile bool imageReceived[50];
+    volatile int size;
 } PNG_BUFFER;
 
 void processInput(char *command, char *value, args* destination);
 void initalizeArguments(args* argument);
-void curlInit(CURL* curlHandlers[], args* arguments);
-void createRequest(CURL* curl);
-void cleanupCurlHandlers(CURL* curlArr[], int numOfCurls);
-void setup_curl_handler(CURL* curlHandler);
+// CURL* curlInit(THREAD_ARGS* threadArgs, RECV_BUF* recv_buf);
+void* createRequest(void* args);
+void initPNGBuffer(PNG_BUFFER* pngBuffer);
+int recv_buf_init(RECV_BUF *ptr, size_t max_size);
 
-PNG_BUFFER pngArray;
+PNG_BUFFER pngBuffer;
 
 /* cURL callbacks */
 // Callback that receives header data.
@@ -63,9 +77,8 @@ int main(int argc, char* argv[]){
 
     args *arguments = malloc(sizeof(args));
 
-    // Initlizaing PNG_BUFFER
-    pngArray.pngs = malloc(((8 + 8 + 4) + (8000)) * 50);
-    pngArray.size = 0;
+    // Initalizing pngBuffer
+    initPNGBuffer(&pngBuffer);
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
@@ -75,10 +88,9 @@ int main(int argc, char* argv[]){
         processInput(argv[i], argv[i+1], arguments);
     }
 
-    pthread_t *p_tids = malloc(sizeof(pthread_t) * arguments->numThreads);
+    pthread_t* p_tids = malloc(sizeof(pthread_t) * arguments->numThreads);
+    THREAD_ARGS* threadArgs = malloc(sizeof(THREAD_ARGS) * arguments->numThreads);
 
-     // Creating numThreads amount of curl handlers to give to each thread. Want to reuse handlers to improve performance.
-    CURL* curlHandlers = (CURL*) malloc(sizeof(CURL) * arguments->numThreads);
 
     // Initilizing Mutex lock
     if (pthread_mutex_init(&lock, NULL) != 0) {
@@ -88,16 +100,27 @@ int main(int argc, char* argv[]){
 
     printf("numThreads: %d | imageSegment: %d\n", arguments->numThreads, arguments->imageNum);
 
-    // Init Curl Handlers
-    curlInit(curlHandlers, arguments);
+    // Create threads
+    for (int i = 0; i < arguments->numThreads; ++i){
+        threadArgs[i].imageToGrab = arguments->imageNum;
+        threadArgs[i].thread_id = i;
+        pthread_create(p_tids + i, NULL, &createRequest, threadArgs + i); 
+    }
 
+    // Join threads
+    for (int i = 0; i < arguments->numThreads; ++i){
+        pthread_join(p_tids[i], NULL);
+    }
 
     free(arguments);
     free(p_tids);
-    free(pngArray.pngs);
+    free(threadArgs);
+
+    for (int i = 0; i < 50; i++){
+        free(pngBuffer.pngs[i]);
+    }
+
     curl_global_cleanup();
-    cleanupCurlHandlers(curlHandlers, arguments->numThreads);
-    free(curlHandlers);
     return 0;
 }
 
@@ -108,66 +131,134 @@ int main(int argc, char* argv[]){
 4) insert png data to proper index on pngArray buff depending on sequence number
 5) increment pngArray counter.
 */
-void createRequest(CURL* curl){
-    while (pngArray.size < 50){
+void* createRequest(void* args){
+    THREAD_ARGS* threadArgs = (THREAD_ARGS*) args;
+    RECV_BUF recv_buf;
 
+    CURL* curl_handle;
+    curl_handle = curl_easy_init();
+
+    if (curl_handle == NULL){
+        printf("Failed to initalize CURL");
+        return NULL;
+    }    
+
+    int imageToGrab = threadArgs->imageToGrab - 1;
+
+    /* specify URL to get */
+    curl_easy_setopt(curl_handle, CURLOPT_URL, urls[0][imageToGrab]);
+
+    /* register write call back function to process received data */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb_curl); 
+    /* user defined data structure passed to the call back function */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&recv_buf);
+
+    /* register header call back function to process received header data */
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_cb_curl); 
+    /* user defined data structure passed to the call back function */
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&recv_buf);
+
+    /* some servers requires a user-agent field */
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    while (pngBuffer.size < 50){
+        recv_buf_init(&recv_buf, BUF_SIZE);
+        CURLcode res = curl_easy_perform(curl_handle);
+
+        if( res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            printf("%lu bytes received in memory %p, seq=%d.\n", \
+                recv_buf.size, recv_buf.buf, recv_buf.seq);
+        }
 
         // Has image, write to pngbuffer
         pthread_mutex_lock(&lock);
+        // Checking if image is already received
+        if (pngBuffer.imageReceived[recv_buf.seq] == false){
+            pngBuffer.pngs[recv_buf.seq] = recv_buf.buf;
+            pngBuffer.imageReceived[recv_buf.seq] = true;
+            pngBuffer.size++;
+        } else {
+            free((&recv_buf)->buf);
+        }
         pthread_mutex_unlock(&lock);
     }
+
+    curl_easy_cleanup(curl_handle);
+
+    return NULL;
 }
 
-// Sets up curl handler for provided url and png data.
-void curlInit(CURL* curlHandlers[], args* arguments){
+// Sets up curl handler. NOT USED
+CURL* curlInit(THREAD_ARGS* threadArgs, RECV_BUF* recv_buf){
 
-    for (int i = 0; i < arguments->numThreads; i++){
-        curlHandlers[i] = curl_easy_init();
+    CURL* curl_handle;
+    curl_handle = curl_easy_init();
 
-        if (curlHandlers[i] == NULL){
-            printf("Failed to initalize CURL");
-            return;
-        }    
+    if (curl_handle == NULL){
+        printf("Failed to initalize CURL");
+        return NULL;
+    }    
 
-        int imageToGrab = arguments->imageNum - 1;
+    int imageToGrab = threadArgs->imageToGrab - 1;
 
-        printf("%s\n", urls[i % 3][imageToGrab]);
+    printf("%s\n", urls[0][imageToGrab]);
 
-        /* specify URL to get */
-        curl_easy_setopt(curlHandlers[i], CURLOPT_URL, urls[i % 3][imageToGrab]);
+    /* specify URL to get */
+    curl_easy_setopt(curl_handle, CURLOPT_URL, urls[0][imageToGrab]);
 
-        /* register write call back function to process received data */
-        // curl_easy_setopt(curlHandlers, CURLOPT_WRITEFUNCTION, write_cb_curl); 
-        /* user defined data structure passed to the call back function */
-        // curl_easy_setopt(curlHandlers, CURLOPT_WRITEDATA, (void *)&pngData);
+    /* register write call back function to process received data */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb_curl); 
+    /* user defined data structure passed to the call back function */
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&recv_buf);
 
-        /* register header call back function to process received header data */
-        // curl_easy_setopt(curlHandlers, CURLOPT_HEADERFUNCTION, header_cb_curl); 
-        /* user defined data structure passed to the call back function */
-        // curl_easy_setopt(curlHandlers, CURLOPT_HEADERDATA, (void *)&pngData);
+    /* register header call back function to process received header data */
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERFUNCTION, header_cb_curl); 
+    /* user defined data structure passed to the call back function */
+    curl_easy_setopt(curl_handle, CURLOPT_HEADERDATA, (void *)&recv_buf);
 
-        /* some servers requires a user-agent field */
-        curl_easy_setopt(curlHandlers[i], CURLOPT_USERAGENT, "libcurl-agent/1.0");
-    }
+    /* some servers requires a user-agent field */
+    curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
+    return curl_handle;
 
 }
 
 // ptr points to the delivered data, and the size of that data is nmemb; size is always 1. 
 size_t write_cb_curl(char *p_recv, size_t size, size_t nmemb, void *p_userdata){
+    size_t realsize = size * nmemb;
+    RECV_BUF *p = (RECV_BUF *)p_userdata;
+ 
+    if (p->size + realsize + 1 > p->max_size) {/* hope this rarely happens */ 
+        /* received data is not 0 terminated, add one byte for terminating 0 */
+        size_t new_size = p->max_size + realsize + max(BUF_INC, realsize + 1);   
+        char *q = realloc(p->buf, new_size);
+        if (q == NULL) {
+            perror("realloc"); /* out of memory */
+            return -1;
+        }
+        p->buf = q;
+        p->max_size = new_size;
+    }
 
-    return 0;
+    memcpy(p->buf + p->size, p_recv, realsize); /*copy data from libcurl*/
+    p->size += realsize;
+    p->buf[p->size] = 0;
+
+    return realsize;
 }
 
 size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata){
+    int realsize = size * nmemb;
+    RECV_BUF *p = userdata;
     
-    return 0;
-}
-
-void cleanupCurlHandlers(CURL* curlArr[], int numOfCurls){
-    for(int i = 0; i < numOfCurls; i++){
-        curl_easy_cleanup(curlArr[i]);
+    if (realsize > strlen(ECE252_HEADER) && strncmp(p_recv, ECE252_HEADER, strlen(ECE252_HEADER)) == 0) {
+        /* extract img sequence number */
+	    p->seq = atoi(p_recv + strlen(ECE252_HEADER));
     }
+
+    return realsize;
 }
 
 /* Takes in the command, the value associated to the command, and a destination "args" to save results to. */
@@ -188,4 +279,31 @@ void processInput(char *command, char *value, args* destination){
 void initalizeArguments(args* argument){
     argument->imageNum = 1;
     argument->numThreads = 1;
+}
+
+void initPNGBuffer(PNG_BUFFER* pngBuffer){
+    for (int i = 0; i < 50; ++i){
+        pngBuffer->imageReceived[i] = false;
+    }
+    pngBuffer->size = 0;
+}
+
+int recv_buf_init(RECV_BUF *ptr, size_t max_size)
+{
+    void *p = NULL;
+    
+    if (ptr == NULL) {
+        return 1;
+    }
+
+    p = malloc(max_size);
+    if (p == NULL) {
+	return 2;
+    }
+    
+    ptr->buf = p;
+    ptr->size = 0;
+    ptr->max_size = max_size;
+    ptr->seq = -1;              /* valid seq should be non-negative */
+    return 0;
 }
