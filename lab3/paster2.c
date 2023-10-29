36e2a3900c52d3ab859692fc8bceb94e4b598717
e2romero <e2romero@uwaterloo.ca>
2023-10-29 15:59:12 -0400

    Updated URL's

--- lab3/paster2.c
+++ lab3/paster2.c
@@ -4,12 +4,28 @@
 #include <stdio.h>
 
 typedef struct _args {
-    int bufferSize;
-    int numProducers;
-    int numConsumers;
-    int numMilliseconds;
-    int imageToFetch;
-} args;
+    int bufferSize; // Buffer to hold image segments. Less than 50.
+    int numProducers; // Number of producers to produce the data.
+    int numConsumers; // Number of consumers to consume/process the data.
+    int numMilliseconds; // Number milliseconds for the consumer to sleep for after processing/consuming the data.
+    int imageToFetch; // Which image segment to fetch
+} args; 
+
+const char* const urls[3][3] = 
+{
+    // Server 1
+    {"http://ece252-1.uwaterloo.ca:2530/image?img=1&part= ", 
+    "http://ece252-1.uwaterloo.ca:2530/image?img=1&part= ", 
+    "http://ece252-1.uwaterloo.ca:2530/image?img=1&part= "},
+    // Server 2
+    {"http://ece252-2.uwaterloo.ca:2530/image?img=1&part= ", 
+    "http://ece252-2.uwaterloo.ca:2530/image?img=2&part= ", 
+    "http://ece252-2.uwaterloo.ca:2530/image?img=3&part= "},
+    // Server 3
+    {"http://ece252-3.uwaterloo.ca:2530/image?img=1&part= ", 
+    "http://ece252-3.uwaterloo.ca:2530/image?img=2&part= ", 
+    "http://ece252-3.uwaterloo.ca:2530/image?img=3&part= "}
+};
 
 args arguments;
 
